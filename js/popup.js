94b69781ec6c3beafa682470f24a34379e32f8f4
Ravvich <ravvich@gmail.com>
2018-03-28 12:47:33 +0300

    исправления js

--- js/popup.js
+++ js/popup.js
@@ -1,13 +1,8 @@
 
 var link = document.querySelector(".modal-button");
 var popup = document.querySelector(".modal");
-var close = document.querySelector(".modal");
 
 link.addEventListener("click", function (evt) {
 	evt.preventDefault();
-	popup.classList.add("modal-show");
-});
-close.addEventListener("click", function (evt) {
-	evt.preventDefault();
-	popup.classList.toggle("modal");
+	popup.classList.toggle("modal-show");
 });
