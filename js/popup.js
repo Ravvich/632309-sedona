d98bd02a1f3e0c68b1620b6b32a5244fe31bb75f
Ravvich <ravvich@gmail.com>
2018-03-31 19:56:54 +0300

    исправление ошибок после первой попытки

--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,12 @@
 
 var link = document.querySelector(".modal-button");
 var popup = document.querySelector(".modal");
+var script = document.querySelector("script");
+
+
+script.onload = function (evt) {
+	popup.classList.add("modal-hide");
+}
 
 link.addEventListener("click", function (evt) {
 	evt.preventDefault();
