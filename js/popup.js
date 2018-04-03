d41294812e5ca9dd4e6a9b0a31b25ab004878575
Ravvich <ravvich@gmail.com>
2018-04-03 16:52:23 +0300

    исправление ошибок

--- js/popup.js
+++ js/popup.js
@@ -2,6 +2,11 @@
 var link = document.querySelector(".modal-button");
 var popup = document.querySelector(".modal");
 var script = document.querySelector("script");
+var form = popup.querySelector("form")
+var datein = popup.querySelector("[name=date-in]");
+var dateout = popup.querySelector("[name=date-out]");
+var adults = popup.querySelector("[name=adults]");
+var child = popup.querySelector("[name=child]");
 
 
 script.onload = function (evt) {
@@ -11,4 +16,12 @@ script.onload = function (evt) {
 link.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	popup.classList.toggle("modal-show");
+	popup.classList.remove("modal-error");
+});
+
+form.addEventListener("submit", function (evt) {
+	if (!datein.value || !dateout.value || !adults.value || !child.value) {
+		evt.preventDefault();
+		popup.classList.add("modal-error");
+	}
 });
